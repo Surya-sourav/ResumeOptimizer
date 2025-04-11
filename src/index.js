7a4370d9fcf01aa22ace529c634cd963c2256d17
Surya <125463872+Surya-sourav@users.noreply.github.com>
2025-04-11 18:15:11 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -13,7 +13,9 @@ const PORT = process.env.PORT || 5000;
 // Middleware
 app.use(cors({
   origin: [
-    'https://resume-optimizer-one.vercel.app/'
+    'https://resume-optimizer-one.vercel.app/',
+    'https://resume-optimizer-git-frontend-surya-souravs-projects.vercel.app/',
+    'https://resume-optimizer-jwilbqf8m-surya-souravs-projects.vercel.app/'
   ]
 }));
 app.use(express.json());
